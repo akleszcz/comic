bd2eedd6edc1e976b79d082dcbd05c176533fed7
Anna Kleszcz <annakleszcz@onet.pl>
2017-10-28 18:16:37 +0200

    require function used to load images with dynamic url

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -3,33 +3,37 @@ import React, { Component } from 'react';
 class Chapter extends Component {
   constructor(props, context) {
     super(props, context);
-    this.importAll = this.importAll.bind(this);
     this.createPageThumbnail = this.createPageThumbnail.bind(this);
   }
 
-  createPageThumbnail(url) {
-    const pageNumber = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.')).substring(3); //img1.png -> 1, img2.png -> 2 etc.
+  createPageThumbnail(filename) {
+    const img = require(`../img/volume${this.props.match.params.volumeNumber}/chapter${this.props.match.params.chapterNumber}/thumbnails/${filename}`);
+    const pageNumber = filename.substring(3, filename.lastIndexOf('.')); //img1.png -> 1, img2.png -> 2 etc.
     const key = `${this.props.match.params.volumeNumber}-${this.props.match.params.chapterNumber}-${pageNumber}`;
-    return <img key = {key} src={url} alt={`Page ${pageNumber}`}/>
+    return <img key = {key} src={img} alt={`Page ${pageNumber}`}/>
   }
 
-  importAll(r) {
+  /*importAll(r) {
     let images = {};
     r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); });
     return images;
-  }
+  }*/
 
   render() {
-    const volumeNumber = this.props.match.params.volumeNumber;
-    const chapterNumber = this.props.match.params.chapterNumber;
-    const imgDirectory = `volume1/chapter1/thumbnails`;// `../img/volume${volumeNumber}/chapter${chapterNumber}/thumbnails`;
-    const images = this.importAll(require.context("../img/" + {imgDirectory}, false, /.png/));
-    const pageThumbnails = Object.keys(images).map(this.createPageThumbnail);
+    //const imgDirectory = `../img/volume1/chapter1/thumbnails`;// `../img/volume${volumeNumber}/chapter${chapterNumber}/thumbnails`;
+    let pageThumbnails = [];
+    for (let i = 1; i < 3; i++) {
+      //const url = `../img/volume1/chapter1/thumbnails/img${i}.png`;
+      const filename = `img${i}.png`;
+      pageThumbnails.push(this.createPageThumbnail(filename));
+    }
+    //const images = this.importAll(require.context("../img/" + {imgDirectory}, false, /.png/));
+    //const pageThumbnails = Object.keys(images).map(this.createPageThumbnail);
     //this.importAll(require.context(`../img/volume${this.props.match.params.volumeNumber}/chapter${this.props.match.params.chapterNumber}/thumbnails`, false, '/.png')).map(this.createPageThumbnail);
     return (
       <div>
         <p>
-          Volume {volumeNumber}, Chapter {chapterNumber}
+          Volume {this.props.match.params.volumeNumber}, Chapter {this.props.match.params.chapterNumber}
         </p>
         <ul>
           {pageThumbnails}
