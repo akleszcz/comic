8c97ba1f214994c263e2c01c79d3a87f67860271
Anna Kleszcz <annakleszcz@onet.pl>
2017-12-02 16:24:36 +0100

    Volume menu tests improved

--- src/__tests__/volume-menu.test.js
+++ src/__tests__/volume-menu.test.js
@@ -4,6 +4,7 @@ import VolumeMenu from '../components/VolumeMenu';
 import Enzyme from "enzyme";
 import { MemoryRouter as Router, withRouter } from 'react-router-dom';
 import { observable } from 'mobx';
+import { Provider } from 'mobx-react';
 //import chapterStore from '../stores/chapterStore';
 
 
@@ -12,7 +13,7 @@ import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("VolumeMenu", function() {
-  let userStore, chapterStore, volumeDetails, volumeNumber;
+  let userStore, chapterStore, volumeDetails, volumeNumber, stores;
   beforeEach(function() {
     userStore = {
       currentUser: {
@@ -21,24 +22,17 @@ describe("VolumeMenu", function() {
       },
       loadingUser: false
     },
-      /*volumesStore = {
-        volumesMap:
-          [
-            {"order_number":1,"title":"Cats","chapters":
-              [{"title":"Maine Coon","id":"rkcJ0TTgG"},{"title":"Siamese","id":"B1DrCTpgM"}]
-            },
-            {"order_number":2,"title":"Dogs","chapters":
-              [{"title":"Labrador","id":"r1_66pTxM"},{"title":"Husky","id":"r19NC6Tlz"},{"title":"Sausage dog","id":"S1grCTaez"}],"id":"rkETjCbeM"
-            }
-          ]
-      },*/
       chapterStore = observable({
-        currentChapter:
-        {"thumbnails":[{"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page1.png","id":"1"},
-        {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page2.png","id":"2"},
-        {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page3.png","id":"3"},
-        {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page4.png","id":"4"},
-        {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page5.png","id":"5"}],"id":"rkcJ0TTgG"},
+        currentChapter: {
+          "thumbnails": [
+            {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page1.png","id":"1"},
+            {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page2.png","id":"2"},
+            {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page3.png","id":"3"},
+            {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page4.png","id":"4"},
+            {"url":"http://comic-rest.azurewebsites.net/images/volume1/chapter2/thumbnails/page5.png","id":"5"}
+          ],
+          "id":"rkcJ0TTgG"
+        },
         get numberOfPages() {
           return this.currentChapter.thumbnails.length;
         },
@@ -64,22 +58,28 @@ describe("VolumeMenu", function() {
           this.deleteChapterCalled = true;
         }
       }),
-      volumeDetails = {"order_number":1,"title":"Cats","chapters":
-        [{"title":"Maine Coon","id":"rkcJ0TTgG"},{"title":"Siamese","id":"B1DrCTpgM"}]
+      volumeDetails = {
+        "order_number":1,
+        "title":"Cats",
+        "chapters": [
+          {"title":"Maine Coon","id":"rkcJ0TTgG"},
+          {"title":"Siamese","id":"B1DrCTpgM"}
+        ]
       },
-      volumeNumber = 1
+      volumeNumber = 1;
+      stores = {
+        userStore,
+        chapterStore
+      };
     });
-    it(' hides chapters when clicked', function() {
-      const wrapper = shallow(<Router><VolumeMenu volumeDetails={volumeDetails} volumeNumber={volumeNumber} chapterStore={chapterStore} userStore={userStore}/></Router>);
-      //console.log(wrapper.debug());//volumeMenu.find(".volume-menu"));
-      //expect(volumeMenu.find(".volume-menu").at(0).text()).toBe("Volume 1: Cats")
-      //const chaptersList = volumeMenu.find('ul');
-      //const header = volumeMenu.find('h2');
-      //console.log({chaptersList});
-    //  expect(chaptersList.children().length).toEqual(volumeDetails.chapters.length);
-      //expect(chaptersList.text()).toEqual('true');
-      /*header.simulate('click');
-      //expect(chaptersList.children().length).toEqual(0);
-      expect(chaptersList.text()).toEqual('');*/
+    it('displays volume title', function() {
+      const wrapper = mount(<Provider {...stores}><Router><VolumeMenu volumeDetails={volumeDetails} volumeNumber={volumeNumber} chapterStore={chapterStore} userStore={userStore}/></Router></Provider>);
+      expect(wrapper.find(".volume-menu").find('h2').at(0).text()).toBe("Volume 1: Cats");
+    });
+    it('hides chapters when clicked', function() {
+      const wrapper = mount(<Provider {...stores}><Router><VolumeMenu volumeDetails={volumeDetails} volumeNumber={volumeNumber} chapterStore={chapterStore} userStore={userStore}/></Router></Provider>);
+      expect(wrapper.find('ul').children().length).toBe(volumeDetails.chapters.length);
+      wrapper.find(".volume-menu").find('h2').simulate('click');
+      expect(wrapper.find('ul').children().length).toBe(0);
     });
 });
--- src/stores/uiStateStore.js
+++ src/stores/uiStateStore.js
@@ -1,6 +1,6 @@
 import { observable, action } from 'mobx';
 
-class UiStateStore {
+export class UiStateStore {
     @observable isLoginModalOpen = false;
     @observable isMenuVisible = true;
 
