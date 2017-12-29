3d36da0e0b19e225da1cd6f096b0d057d34180c1
Anna Kleszcz <annakleszcz@onet.pl>
2017-12-29 15:24:06 +0100

    New chapter input font color changed

--- src/components/AddChapterView.js
+++ src/components/AddChapterView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import Add from './icons/Add';
+import '../css/AddChapterView.scss';
 
 @inject('chapterStore')
 @observer
